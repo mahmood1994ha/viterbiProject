bfcef74d088e2852c88d0ad311690ef87e31336d
mahmoud.bazzal <mahmoud.bazzal001@stud.fh-dortmund.de>
2019-06-30 11:45:47 +0000

    rearranged project

--- src/ModAdder.h
+++ src/ModAdder.h
@@ -23,33 +23,4 @@ SC_MODULE(modAdder){
 	     sensitive << in1 << in2;  // sensitivity list
 	   }
 };
-
-SC_MODULE(shiftReg){
-	 sc_in<bool> dataIn;/**< input bits to modulo adder */
-	 sc_in<bool> clk;
-	 sc_out<sc_uint<3>> dataOut;/**< output bit from modulo adder */
-
-	 void shift(){
-		 sc_uint<3> reg;
-		 dataOut.write(0);
-		 while(1){
-			 wait();/**< wait for signal on sensitivity list*/
-			 reg = dataOut;
-			 reg[2] = reg[1];
-			 reg[1] = reg[0];
-			 reg[0] = dataIn;
-			 wait(2, SC_NS); /**< delay to produce outputs.*/
-			 dataOut.write(reg);
-		 }
-	 }
-
-	 SC_CTOR(shiftReg)          // constructor for modulo Adder
-	   {
-	     SC_THREAD(shift);  // register modAdder with kernel
-	     sensitive << clk.pos();  // sensitivity list
-	   }
-};
-
-
-
 #endif /* MODADDER_H_ */
--- src/ShiftReg.h
+++ src/ShiftReg.h
@@ -7,8 +7,33 @@
 
 #ifndef SHIFTREG_H_
 #define SHIFTREG_H_
+#include "systemc.h"
 
+SC_MODULE(shiftReg){
+	 sc_in<bool> dataIn;/**< input bits to modulo adder */
+	 sc_in<bool> clk;
+	 sc_out<sc_uint<3>> dataOut;/**< output bit from modulo adder */
 
+	 void shift(){
+		 sc_uint<3> reg;
+		 dataOut.write(0);
+		 while(1){
+			 wait();/**< wait for signal on sensitivity list*/
+			 reg = dataOut;
+			 reg[2] = reg[1];
+			 reg[1] = reg[0];
+			 reg[0] = dataIn;
+			 wait(2, SC_NS); /**< delay to produce outputs.*/
+			 dataOut.write(reg);
+		 }
+	 }
+
+	 SC_CTOR(shiftReg)          // constructor for modulo Adder
+	   {
+	     SC_THREAD(shift);  // register modAdder with kernel
+	     sensitive << clk.pos();  // sensitivity list
+	   }
+};
 
 
 
