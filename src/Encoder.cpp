adb01c75d3e99fbe2b1040b3075c02d367590aab
ram.govindarajan <ram.govindarajan001@stud.fh-dortmund.de>
2019-07-12 16:48:11 +0200

    Encoder 2 bit Working

--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -9,7 +9,7 @@ void Encoder::encode()
 
 	{
 		wait();
-		for (int i = 3; i>0; i--)
+		for (int i = 2; i>0; i--)
 		{
 			shiftregister[i] = shiftregister[i - 1];
 
@@ -17,17 +17,18 @@ void Encoder::encode()
 
 
 		shiftregister[0] = input;
-
-		cout <<"Input = "<<input<<"\t\t";
-		wait(200, SC_NS);
-		cout <<"Encoder Bit= ";
-		for (int i = 0; i<4; i++)
+		cout <<"EncoderBit = ";
+		for (int i = 0; i<3; i++)
 		{
 			cout<<shiftregister[i];
 
 		}
-		out1 = shiftregister[3]  ^shiftregister[2]  ^ shiftregister[1] ^ shiftregister[0];
-		out2 = shiftregister[3]  ^shiftregister[1] ^ shiftregister[0];
+
+
+		cout <<"\t\tInput = "<<input<<"\t\t";
+		wait(200, SC_NS);
+		out1 = shiftregister[2]  ^ shiftregister[1] ^ shiftregister[0];
+		out2 = shiftregister[2] ^ shiftregister[0];
 		output1.write(sc_bit(out1));
 		output2.write(sc_bit(out2));
 		cout <<"\t\t Output ==>"<<out1<<out2<<"\t\t"<<"\n"<<endl;
